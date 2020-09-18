fb74691fcc4c87d43bfdc447bbb42e25a6c31175
fcj <2309134857@qq.com>
2020-09-18 17:55:09 +0800

    提交查询会员全部用例

--- bin/run_multiprofile.py
+++ bin/run_multiprofile.py
@@ -3,7 +3,7 @@ import unittest
 from setting import *
 from BeautifulReport import BeautifulReport
 #discover运行的路径
-discover=unittest.defaultTestLoader.discover(CASE_PATH,'M*.py')
+discover=unittest.defaultTestLoader.discover(CASE_PATH,'test_M*.py')
 time_now=time.strftime('%Y%m%d%H%M%S')
 #生成报告的名字
 report_name="小白接口-{}.html".format(time_now)
--- cases/test_MultiProfile.py
+++ cases/test_MultiProfile.py
@@ -3,6 +3,8 @@
 """
 import unittest
 import requests,ddt,os
+
+
 from setting import *
 
 
@@ -11,7 +13,7 @@ class MultiProfile(unittest.TestCase):
     @ddt.file_data(os.path.join(DATA_PATH,'MultiProfile.yaml'))
     def test_multiprofile(self,**payload):
         self._testMethodDoc=payload.get('detail')
-        base_url=payload.get('url') #取url
+        base_url=payload.get('url') #url
         args=payload.get('data') #获取需要传递得参数
         if payload.get('method')=='get':
             resp=requests.get(url=base_url,params=args).json()
@@ -26,3 +28,18 @@ class MultiProfile(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
+
+
+# #uuid数据
+# UUid1='676003847A6FAF10908B6A6184A84B2C'
+# Uuid2='3FE6E35B5A4DC896CB8034E2F08A1473'
+# UUid3='4EC1E8184D8C377B2314EA327AD60354'
+# s='App. User.MultiProfile'#请求ur
+# base_url='http://hn216.api.yesapi.cn'#请求app_key
+# base_key='BE7FE3D8B276951BCBDDDB56C4D58479'#请求数据
+# args={
+#     's':'App. User.MultiProfile',
+#     'app_key':base_key,
+#     'uuids':'676003847A6FAF10908B6A6184A84B2C' }
+# re=requests.get(url='http://hn216.api.yesapi.cn',params=args)
+# print(re.text)
